a1d15775730758cd4eb9b40ee24bddec60f17673
Bruno <seletonurb@gmail.com>
2018-07-27 18:02:21 -0300

    Promote BookList component to container and connect it to Books reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: "Javascript: The Good Parts" },
+    { title: "Lord of the Rings" },
+    { title: "Harry Potter" },
+    { title: "Star Wars" }
+  ];
+}
