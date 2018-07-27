5a2653c9afea71a143f704cc692713d418d89111
Bruno <seletonurb@gmail.com>
2018-07-27 17:16:38 -0300

    Append BookList component to app root view (main page)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+
+import BookList from "./containers/book-list";
 
 // Create a new component. This component should produce some HTML
 const App = function() {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <BookList />
+    </div>
+  );
 };
 // Take this component's generated HTML and put it n the page (in the DOM)
-ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector(".container"));
