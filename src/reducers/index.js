d8f9b62984086c2621a7a03405511bae065fda3e
Bruno <seletonurb@gmail.com>
2018-07-28 19:44:56 -0300

    Fix typo in Root Reducer property

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,7 +4,7 @@ import ActiveBookReducer from "./reducer_active_book";
 
 const rootReducer = combineReducers({
   books: BooksReducer,
-  ativeBook: ActiveBookReducer
+  activeBook: ActiveBookReducer
 });
 
 export default rootReducer;
